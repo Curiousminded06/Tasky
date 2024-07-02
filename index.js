0e571af6720e47bcd463dbe32797d4c907cc835b
Sahil <sahil2456.rsh@gmail.com>
2024-07-02 22:10:38 +0530

    add delete feature

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const taskContainer = document.querySelector(".task__container");
-
+let globalStore = [];//load local storage data to array,to make changes in global array after deletion takes place
 const generateNewCard = (taskData) => `
-<div class="col-md-6 col-lg-4" id=${taskData.id}>
+<div class="col-md-6 col-lg-4">
 <div class="card">
   <div class="card-header d-flex justify-content-end gap-2">
     <button type="button" class="btn btn-outline-success">
       <i class="fas fa-pencil-alt"></i>
     </button>
-    <button type="button" class="btn btn-outline-danger">
-      <i class="fas fa-trash-alt"></i>
+    <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this,arguments)" >
+      <i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"></i>
     </button>
   </div>
   <img
@@ -32,18 +32,20 @@ const generateNewCard = (taskData) => `
 </div>
 `;
 //const globalStore = [];//store all task data in form of object,refresh removes the old list of array
-const globalStore = [];//load local storage data to array
+
 
 const loadInitialCardData=()=>{
     //localstoragemto get tasky card data
     const getCardData = localStorage.getItem("tasky");
     //convert from string to normal object
-    const {cards}=JSON.parse(getCardData);
-    //loop over those array of task object to create html card,inject it to DOM
+    const {cards}=JSON.parse(getCardData); //destructuring only card prop.
+    //loop over those array of task object to create html card
     cards.map((cardObject) =>{ 
+        //inject it to DOM
         taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
         //update our globalStore
-        globalStore.push(taskData);})
+        globalStore.push(taskData);}
+    )
     
 };
 
@@ -65,7 +67,32 @@ localStorage.setItem("tasky",JSON.stringify({cards:globalStore})); //an object
 
 };
 
+const deleteCard=(event) =>{
+   
+    event=window.event;
+    //id
+    const targetID=event.target.id;
+    //button clicked or icon
+    const tagname=event.target.tagName; //BUTTON
 
+
+    //match the id of the lement with the id inside the globalStore
+    globalStore = globalStore.filter((cardObject) =>cardObject.id !== targetID);//not equal to
+    localStorage.setItem("tasky",JSON.stringify({cards:globalStore})); //an object
+
+    
+    
+    //we have updated array of cards
+    //contact parent 
+    if(tagname=='BUTTON'){
+        return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode)//3 steps for button
+    }else {
+        return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode)//4 steps for icon
+
+    }
+   // taskContainer.removeChild() -> to delete it ,we need to reach parent in how many steps 
+
+};
 
 
 
@@ -95,3 +122,4 @@ localStorage.setItem("tasky",JSON.stringify({cards:globalStore})); //an object
 
 //features 
 //delete the card
+//id approach-> to access card when button is clicked
